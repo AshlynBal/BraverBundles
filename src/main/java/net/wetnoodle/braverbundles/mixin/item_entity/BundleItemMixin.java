45887002447dae273bc6701fe9db027212c124fe
Wet Noodle <kluski42@gmail.com>
2024-12-20 18:20:19 -0500

    Added sounds to collecting item entities

--- src/main/java/net/wetnoodle/braverbundles/mixin/item_entity/BundleItemMixin.java
+++ src/main/java/net/wetnoodle/braverbundles/mixin/item_entity/BundleItemMixin.java
@@ -3,6 +3,7 @@ package net.wetnoodle.braverbundles.mixin.item_entity;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
@@ -14,11 +15,20 @@ import net.minecraft.world.phys.HitResult;
 import net.wetnoodle.braverbundles.BraverBundles;
 import net.wetnoodle.braverbundles.config.BBConfig;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BundleItem.class)
 public abstract class BundleItemMixin {
+    @Shadow
+    private static void playInsertSound(Entity entity) {
+    }
+
+    @Shadow
+    private static void playInsertFailSound(Entity entity) {
+    }
+
     @WrapOperation(method = "dropContent(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/BundleItem;dropContent(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;)Z"))
     boolean braverBundles$dropContent(BundleItem instance, ItemStack itemStack, Player player, Operation<Boolean> original) {
@@ -38,26 +48,31 @@ public abstract class BundleItemMixin {
             BraverBundles.LOGGER.info(entityHitResult.getEntity().toString());
             if (entityHitResult.getEntity() instanceof ItemEntity itemEntity) {
                 success = true;
-                braverBundles$sendEntityToBundle(itemStack, itemEntity);
+                if (braverBundles$sendEntityToBundle(itemStack, itemEntity)) {
+                    playInsertSound(player);
+                } else {
+                    playInsertFailSound(player);
+                }
             }
         }
         return success;
     }
 
     @Unique
-    private ItemStack braverBundles$sendEntityToBundle(ItemStack bundleStack, ItemEntity itemEntity) {
+    private boolean braverBundles$sendEntityToBundle(ItemStack bundleStack, ItemEntity itemEntity) {
         BundleContents bundleContents = bundleStack.get(DataComponents.BUNDLE_CONTENTS);
-        if (bundleContents == null) return bundleStack;
+        if (bundleContents == null) return false;
         int space = bundleContents.weight().getDenominator() - bundleContents.weight().getNumerator();
         BundleContents.Mutable mutable = new BundleContents.Mutable(bundleContents);
+        int inserted = 0;
         if (space >= 1) {
             ItemStack stack = itemEntity.getItem().copy();
-            int inserted = mutable.tryInsert(stack);
+            inserted = mutable.tryInsert(stack);
             if (inserted != 0) {
                 itemEntity.setItem(stack);
             }
         }
         bundleStack.set(DataComponents.BUNDLE_CONTENTS, mutable.toImmutable());
-        return bundleStack;
+        return inserted != 0;
     }
 }
